52c53444682d9afaba508c9d4bc23b82c2b6d465
aishwarya14m <aishwarya14m@gmail.com>
2020-11-19 00:15:36 +0530

    Add files via upload

---
+++ bubblesMovement.js
@@ -0,0 +1,15 @@
+var grid = d3.select("div").append("svg").attr("width",2000).attr("height",1000);
+
+//grid.append("rect").attr("x",10).attr("y",10).attr("width",50).attr("height", 50).attr("fill","red");
+    
+
+document.getElementById("container").addEventListener("mouseover", function(e){
+    //grid.append("rect").attr("width",50).attr("height", 50).attr("fill","black");
+    grid.append("circle")
+   .attr("cx", e.clientX)
+   .attr("cy", e.clientY)
+   .attr("r", 20)
+   .attr("fill", "rgb("+Math.floor(Math.random() * (10))+","+e.clientY+","+(e.clientX*(2))+")")
+    console.log(e.clientX+" and "+e.clientY);
+    
+})
